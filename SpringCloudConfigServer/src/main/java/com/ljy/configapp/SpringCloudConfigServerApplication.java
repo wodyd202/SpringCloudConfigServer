a08b126062de28ca4dbbf5092d28b6497dc732b3
wodyd202 <wodyd202@naver.com>
2021-02-07 00:09:57 +0900

    - hello user url 매핑 정보 설정

--- SpringCloudConfigServer/src/main/java/com/ljy/configapp/SpringCloudConfigServerApplication.java
+++ SpringCloudConfigServer/src/main/java/com/ljy/configapp/SpringCloudConfigServerApplication.java
@@ -3,10 +3,21 @@ package com.ljy.configapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableConfigServer
+@RestController
 public class SpringCloudConfigServerApplication {
+
+	@GetMapping("{username}")
+	public ResponseEntity<String> hello(@PathVariable String username) {
+		return new ResponseEntity<>("hello ~~~ " + username, HttpStatus.OK);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
